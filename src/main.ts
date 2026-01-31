503bcbaf903e23717ddc84b8e2359c4e3f8f08eb
mostafa-mahmood <moustafakhader123@gmail.com>
2026-01-31 16:31:12 +0200

    chore: add db module and update path configs

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,6 @@ import { AppModule } from './app.module';
 
 (async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    app.listen(process.env.PORT ?? 8000);
+    await app.listen(process.env.PORT ?? 8000);
+    console.log(`Application is running on PORT ${process.env.PORT ?? 8000}`);
 })();
